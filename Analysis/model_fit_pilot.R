77d2ba6a99352f939398c5084fdd91772973e024
siebe2208 <siebeeveraerts1@gmail.com>
2025-12-08 15:28:03 +0100

    updates

--- Analysis/model_fit_pilot.R
+++ Analysis/model_fit_pilot.R
@@ -18,7 +18,7 @@ entropy = function(p){
 }
 
 ## Data Cleaning
-data = read.csv(choose.files())
+data = read.csv(here("Analysis", "RDM_reportz_sub0_2.csv"))
 data_main = data[data$Trialtype == "Main",]
 data_main = data_main[data_main$scale == "conf", ]
 data_main$SR_conf = as.numeric(data_main$SR_conf)
@@ -33,7 +33,7 @@ data_main <- data_main %>%
   mutate(coherence = ifelse(cor_resp == "down", -(coherence), coherence))
 data_main$up = ifelse(data_main$resp == "up", 1, 0)
 
-#Data extraction
+## Data extraction
 N = nrow(data_main)
 binom_y = data_main$cor
 RT = data_main$RTdec
@@ -46,19 +46,20 @@ ACC = data_main$cor
 
 data_list = list(N = N, RT = RT, Conf = conf, X = x, minRT = minRT, ACC = ACC, binom_y = binom_y)
 
-model = cmdstan_model(here("models", "single_sub_nocopula.stan"))
+model = cmdstan_model(here("Analysis", "single_sub_nocopula.stan"))
 
 #fit = model$sample(data = data_list, parallel_chains = 4)
 #fit$save_object(here("models", "fit_pilot_siebe.rds"))
-fit <- readRDS("~/psychologie 1e Master/internship/models/fit_pilot_siebe.rds")
+fit <- readRDS(here("Analysis", "fit_pilot_siebe.rds"))
 
 ## Fit extraction Type 1
 x = seq(-1,1, length.out = 200)
 draw_id = sample(1:4000,100)
 
-posterior = as_draws_df(fit$draws(c("alpha","beta","lapse"))) %>%
-  mutate(beta = exp(beta)) %>%
-  select(-contains(".")) %>% mutate(draw = 1:n(),
+draws = as_draws_df(fit$draws(c("alpha","beta","lapse", "rt_int", "rt_slope", "rt_ndt", "rt_prec"))) %>% select(-contains("."))
+
+posterior = draws %>%
+  mutate(beta = exp(beta)) %>% mutate(draw = 1:n(),
   x = list(x)) %>% unnest()
 
 draws_psych = posterior %>% filter(draw%in% draw_id) %>% 
@@ -70,19 +71,34 @@ med_beta = median(posterior$beta)
 med_lapse = median(posterior$lapse)
 y_pred_med = psycho_ACC(x, med_alpha, med_beta, med_lapse)
 
-# residuals 
-residuals = data_main %>% mutate(model_pred = psycho_ACC(coherence, med_alpha, med_beta, med_lapse))%>%
-  mutate(residual = up-model_pred) %>% pull(residual)
+# residuals Type 1
+data_main = data_main %>% mutate(model_pred = psycho_ACC(coherence, med_alpha, med_beta, med_lapse))%>%
+  mutate(residual = up-model_pred) 
 
 ## Fit extraction RT
-draws_RT = as_draws_df(fit$draws(c("rt_int", "rt_slope", "rt_ndt"))) %>%  
-  select(-contains(".")) %>% mutate(draw = 1:n(),
-  x = list(x))%>% unnest()
-med_rt_int = median(draws_RT$rt_int)
-med_rt_slope = median(draws_RT$rt_slope)
-med_rt_ndt = median(draws_RT$rt_ndt)
-entropy_pred = entropy(y_pred_med)
-RT_pred = exp(med_rt_int + med_rt_slope*entropy_pred) + med_rt_ndt
+posterior_1 = posterior %>% mutate(theta = psycho_ACC(x, alpha, beta, lapse), entropy = entropy(theta)) %>%
+  mutate(rt_mu = rt_int + rt_slope*entropy) %>% mutate(rt_mu_ndt = rt_mu - rt_ndt) %>% rowwise() %>%
+  mutate(rt_muEXP = exp(rt_mu)+rt_ndt) %>% 
+  mutate(RT_sim = rlnorm(1, meanlog = rt_mu, sdlog = rt_prec) +rt_ndt) 
+
+
+
+summary = posterior_1 %>% select("x", "draw", "rt_mu", "rt_prec", 'rt_ndt', "rt_mu_ndt", "RT_sim", "rt_muEXP") %>% arrange(x) %>% group_by(x) %>%
+  summarise(
+    med_rt = median(rt_muEXP, na.rm = TRUE),
+    low = quantile(rt_muEXP, probs = 0.05),
+    high = quantile(rt_muEXP, probs = 0.95),
+    sd_low = quantile(RT_sim, probs = 0.05),
+    sd_high = quantile(RT_sim, probs = 0.95)
+  )
+
+## Residuals RT type 1
+data_main = data_main %>% rowwise() %>%
+  mutate(RT_pred = list(draws$rt_int + draws$rt_slope * entropy(model_pred))) %>% 
+  mutate(RT_pred = list(exp(RT_pred) + draws$rt_ndt)) %>% 
+  mutate(RT_pred = mean(RT_pred)) %>%
+  mutate(residuals_RT = RTdec -RT_pred)
+  
 
 ## Plots binary choice
 type_1_p = ggplot() +
@@ -96,9 +112,8 @@ type_1_p = ggplot() +
                   axis.text = element_text(size = 14), 
                   axis.line = element_line(size = 1.5, color = "black"))
 
-type_1_res = data.frame(x = data_main$coherence, y=residuals)%>% 
-              ggplot()+
-              geom_point(aes(x=x,y=y), color = "grey", size = 4, shape = 16)+
+type_1_res =  ggplot()+
+              geom_point(data = data_main, aes(x=coherence,y= residual), color = "grey", size = 4, shape = 16)+
               geom_hline(yintercept = 0, linetype = "longdash", color = "black", size = 1.2)+
               labs(x = "Coherence", y = "Obs - Pred", title = "residuals Type 1") +
               theme_minimal()+
@@ -109,14 +124,28 @@ type_1_res = data.frame(x = data_main$coherence, y=residuals)%>%
 type_1_p + type_1_res
 
 ## Plots reaction times
-ggplot()+
-  geom_point(data = data_main, aes(x=coherence, y = RTdec), 
-            color = "grey", size =2.5, alpha =0.5)+
-  geom_line(aes(x = x, y = RT_pred), color = "black", size = 1, alpha = 1)+
-  labs(x = "Coherence", y = "Reaction times", title = "RT Type 1")+
-  theme_minimal()+
-  theme(plot.title = element_text(hjust = 0.5, size = 16),
-        axis.title = element_text(size = 16),                 
-        axis.text = element_text(size = 14), 
-        axis.line = element_line(size = 1.5, color = "black"))
+RT_p = ggplot()+
+        geom_point(data = data_main, aes(x=coherence, y = RTdec), 
+                  color = "blue", size =2.5, alpha =0.5)+
+        geom_ribbon(data= summary, aes(x=x, ymin =sd_low, ymax = sd_high), fill ="grey", alpha = 0.3)+
+        geom_ribbon(data = summary, aes(x = x, ymin = low, ymax = high), fill = "grey", alpha= 0.5)+
+        geom_line(data = summary,aes(x = x, y = med_rt), color = "black", size = 1.2, alpha = 1)+
+        labs(x = "Coherence", y = "Reaction times", title = "RT Type 1")+
+        theme_minimal()+
+        theme(plot.title = element_text(hjust = 0.5, size = 16),
+              axis.title = element_text(size = 16),                 
+              axis.text = element_text(size = 14), 
+              axis.line = element_line(size = 1.5, color = "black"))
+
+RT_res = ggplot() + 
+          geom_point(data = data_main, aes(x=coherence,y= residuals_RT), color = "grey", size = 4, shape = 16)+
+          geom_hline(yintercept = 0, linetype = "longdash", color = "black", linewidth = 1.2)+
+          labs(x = "Coherence", y = "Obs - Pred", title = "residuals RT Type 1") +
+          theme_minimal()+
+          theme(plot.title = element_text(hjust = 0.5, size = 16),
+                axis.title = element_text(size = 16),                 
+                axis.text = element_text(size = 14), 
+                axis.line = element_line(size = 1.5, color = "black"))
+RT_p + RT_res
+  
   
